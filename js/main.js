685d6b60749b5bb0c06302594e02e70183829edc
Joshua-Talltree <73854373+Joshua-Talltree@users.noreply.github.com>
2020-11-05 16:56:55 -0600

    New JS/HTML files add

---
+++ js/main.js
@@ -0,0 +1,12 @@
+"use strict";
+console.log("Hello from Codeup");
+
+alert("You just won a new iPad!");
+alert("Just kidding, of course, email and phone number please");
+
+// the following line will show the OK/CANCEL confirm dialog
+var confirmed = confirm('Are you sure you want to do XYZ?');
+console.log(confirmed); // will be either true or false
+
+var userInput = prompt('Please type something:');
+console.log('The user entered: ' + userInput);
