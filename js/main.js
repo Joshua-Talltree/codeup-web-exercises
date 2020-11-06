dc24910587dca8b3d6a6feba7da72f385f54d036
Joshua-Talltree <73854373+Joshua-Talltree@users.noreply.github.com>
2020-11-06 16:43:14 -0600

    My latest add

--- js/main.js
+++ js/main.js
@@ -1,12 +1,33 @@
-"use strict";
-console.log("Hello from Codeup");
+// "use strict";
+// console.log("Hello from Codeup");
+//
+// alert("You just won a new iPad!");
+// alert("Just kidding, of course, email and phone number please");
+//
+// // the following line will show the OK/CANCEL confirm dialog
+// var confirmed = confirm('Are you sure you want to do XYZ?');
+// console.log(confirmed); // will be either true or false
+//
+// var userInput = prompt('Please type something:');
+// console.log('The user entered: ' + userInput);
+//
+// function myFunction(input) {
+//     var output = glue; /* do something to calculate output */;
+//     return output;
+// }
+//
+// // a function with no parameters and no return value
+// function sayHello() {
+//     alert('hello there Josh');
+// }
+//
+// sayHello("codeup"); // "codeup" is ignored
 
-alert("You just won a new iPad!");
-alert("Just kidding, of course, email and phone number please");
+// function someFunctionsNoParameters(){
+//     return alert("Hello there, Kalypso!");
 
-// the following line will show the OK/CANCEL confirm dialog
-var confirmed = confirm('Are you sure you want to do XYZ?');
-console.log(confirmed); // will be either true or false
 
-var userInput = prompt('Please type something:');
-console.log('The user entered: ' + userInput);
+function bigNews() {
+    return alert(bigNews + "Coming Soon");
+}
+
