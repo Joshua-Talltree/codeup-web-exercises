e603ecc864f91846094b999e6fe9d3408a117abd
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2020-11-13 14:57:14 -0600

    New loops to add for the day

---
+++ break_and_continue.js
@@ -0,0 +1,34 @@
+"use strict";
+
+// Prompt the user for an odd number between 1 and 50. Use a loop and a break statement to continue prompting the user if they enter invalid input.
+
+
+do {
+    var userInput = Number(prompt("Pick an odd number between 1 and 50"));
+    if (userInput % 2 === 0) {
+        alert(userInput + " is not odd, please try again")
+    } else if (userInput > 50) {
+        alert(userInput + " is greater than 50, please try again")
+    } else if (userInput < 1) {
+        alert(userInput + " is less than 1, please try again")
+    } else {
+        alert("You picked a good one!");
+        break;
+    }
+} while (true)
+
+// 3. Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number the user entered.
+
+do {
+    var userInput = Number(prompt("Pick an odd number between 1 and 50"));
+    if (userInput % 2 === 0) {
+        alert(userInput + " is not odd, please try again")
+    } else if (userInput > 50) {
+        alert(userInput + " is greater than 50, please try again")
+    } else if (userInput < 1) {
+        alert(userInput + " is less than 1, please try again")
+    } else {
+        alert("You picked a good one!");
+        break;
+    }
+} while (true)
--- for_loops.js
+++ for_loops.js
@@ -1,33 +1,63 @@
 "use strict";
 
-// create a for loop that multiplies 7 by numbers 1 - 10.
+// 2. create a for loop that multiplies 7 by numbers 1 - 10.
 
-var result = '\n';
-var j = 7;
-for (var i = 1; i < 11; i++) {
-    result += (i*j) + ' ';
-    result += '\n';
+var result = 'n';
+var num = 7;
+
+function showMultiplicationTable(j) {
+    for (var i = 1; i < 11; i++) {
+        var n1 = j * i;
+        console.log(num + " x " + i + " = " + n1);
+
+    }
+
 }
 
+showMultiplicationTable(num)
 
-console.log(result);
-
-// Use for loop to create random number between 20 and 200
+// 3. Use for loop to create random number between 20 and 200
 
 // This is how you get a random number between 50 and 100
-var allCones = Math.floor(Math.random() * 50) + 50;
-// This expression will generate a random number between 1 and 5
-Math.floor(Math.random() * 5) + 1;
+// var ranNum = Math.floor(Math.random() * 180) + 20;
+// // This expression will generate a random number between 1 and 5
+// Math.floor(Math.random() * 5) + 1;
 
-for (var allCones = a = 1; a < 100; a++) {
-
-    if (a % 2 !== 0) {
-        // number isn't even
-        // odd numbers aren't as cool
-        // skip the rest of the loop and continue with the next iteration
-        continue;
+for (var num1 = 0; num1 < 10; num1++) {
+    var ranNum = Math.floor((Math.random()* 180) +20);
+    if (ranNum % 2 !== 0) {
+        console.log(ranNum + " is odd");
+    } else { (ranNum % 2 === 0);
+        console.log(ranNum + "" + " is even");
     }
+
+
 }
 
-console.log(allCones + a);
+// 4. Create a for loop that uses console.log to create the output shown below.
+
+var i,j
+
+for(i=1;i<=10;i++) {
+    console.log(i);
+{
+    for(j=1;j<=i;j++);
+        console.log(j);
+
+}
+
+}
+
+// 5. Create a for loop that uses console.log to create the output shown below.
+
+
+for (var n2 = 1; n2 < 10; n2++) {
+    var string = n2.toString();
+    console.log(Number(string.repeat + n2));
+}
+for (var n3 = 100; n3 > 0; n3--) {
+    if (n3 % 5 === 0) {
+        console.log(n3);
+    }
+}
 
