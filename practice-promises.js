b1591f5304d15f92e19a3eb88b5e32adc6a628a3
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2021-01-21 13:48:26 -0600

    making sure everything is committed...

---
+++ practice-promises.js
@@ -0,0 +1,63 @@
+// // const myPromise = fetch('https://api.github.com/users');
+// // myPromise.then(response => console.log(response));
+// // myPromise.catch(error => console.error(error));
+//
+//
+// const getLastCommitDate = (username) => {
+//     return fetch()
+// }
+// // function makeRequest() {
+// //     return new Promise((resolve, reject) => {
+// //         setTimeout(() => {
+// //             if (Math.random() > 0.1) {
+// //                 resolve('Here is your data: ...');
+// //             } else {
+// //                 reject('Network Connection Error!');
+// //             }
+// //         }, 1500);
+// //     });
+// //
+// // }
+// // console.log(makeRequest());
+// //
+// //
+// // // const myPromise = fetch('https://api.github.com/users')
+// // //     .then(response => console.log(response))
+// // //     .catch(error => console.error(error));
+// // //
+// // // const swapiurl = "https://swapi.dev/api/planets/"
+// // // const pokeapiurl = "https://pokeapi.co//api/v2"
+// //
+// // // document.getElementById("press").addEventListener("click", ()=>{
+// // //     let id = document.getElementById("input").value;
+// // //     console.log("fetching " + swapiurl + id);
+// // // })
+// //
+// // // Promise.resolve('one').then((one) => {
+// // //     console.log(one);
+// // //     return 'two';
+// // // }).then((two) => {
+// // //     console.log(two);
+// // //     return 'three';
+// // // }).then((three) => {
+// // //     console.log(three);
+// // // });
+// // //
+// // // function makeRequest() {
+// // //     return new Promise((resolve, reject) => {
+// // //         setTimeout(() => {
+// // //             if (Math.random() > 0.1) {
+// // //                 resolve('Here is your data: ...');
+// // //             } else {
+// // //                 reject('Network Connection Error!');
+// // //             }
+// // //         }, 1500);
+// // //     });
+// // // }
+// // //
+// // // const request = makeRequest();
+// // // console.log(request); // pending promise
+// // // request.then(message => console.log('Promise resolved!', message));
+// // // // if resolved, will log "Promise resolved!" and "Here is your data: ..."
+// // // request.catch(message => console.log('Promise rejected!', message));
+// // // if rejected, will log "Promise rejected!" and "Network Connection Error!"
